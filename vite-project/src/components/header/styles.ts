0170f49b5d3e6b85f85df87e9ebefede05553631
pedro.mujica1 <miottopedro@gmail.com>
2023-10-30 10:03:49 -0300

    adding styles in ts file

--- vite-project/src/components/header/styles.ts
+++ vite-project/src/components/header/styles.ts
@@ -7,7 +7,7 @@ export const Container = styled.header`
     background-color: red;
     align-items: center;
     justify-content: center;
-    height: 350px;
+    height: 198px;
     display: flex;
          
 `;
@@ -15,5 +15,22 @@ export const Container = styled.header`
 //Variável Content
 export const Content = styled.div`
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .details{
+        h1{
+            font-size:32px;
+            color:#fff;
+        }
+        h2{
+            font-size:16px;
+            color:#fff;
+            font-weight:400;
+            opacity: 0.9;
+            margin-top:6px;
+        }
+    }
 
     `
