eef89390d53eb64070582020bd92bea49ffacebe
pedro.mujica1 <miottopedro@gmail.com>
2023-10-30 08:20:35 -0300

    primeiros codigos React

---
+++ vite-project/src/components/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header(){
+    return (
+        <div>
+            <h1>Pedidos</h1>
+            <p>Helllo world from React JS</p>
+            <h2>Acompanhamento de Pedidos</h2>
+        </div>
+    );
+}
