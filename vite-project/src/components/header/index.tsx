8d2ed94ed3eca0e0553affe1feda337ac5e630d0
pedro.mujica1 <miottopedro@gmail.com>
2023-10-30 09:15:32 -0300

    adding Content component

--- vite-project/src/components/header/index.tsx
+++ vite-project/src/components/header/index.tsx
@@ -1,13 +1,20 @@
 import logo from '../../assets/logo.svg'
+import { Container,Content } from './styles';
 
 export function Header(){
     return (
-        <div>
-            <p>Helllo world from React JS</p>
-            <h1>Pedidos</h1>
-            <h2>Acompanhamento de Pedidos</h2>
-            <img src={logo} alt="Imagem Logo" />
-        </div>
+        <Container>
+            <Content>
+
+                <div>
+                    <p>Helllo world from React JS</p>
+                    <h1>Pedidos</h1>
+                    <h2>Acompanhamento de Pedidos</h2>
+                    
+                </div>
+                <img src={logo} alt="Imagem Logo" />
+            </Content>
+        </Container>
         
     );
 }
---
+++ vite-project/src/components/header/styles.ts
@@ -0,0 +1,19 @@
+//Instalar Extensão styled-components-snippets
+//INSTALAR EXTENSÃO VSCODE STYLED COMPONENTS
+import { styled } from "styled-components";
+
+//Variável Container
+export const Container = styled.header`
+    background-color: red;
+    align-items: center;
+    justify-content: center;
+    height: 350px;
+    display: flex;
+         
+`;
+
+//Variável Content
+export const Content = styled.div`
+    width: 100%;
+
+    `
